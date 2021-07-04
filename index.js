a880bf245a4ff5c080e7f3a2861e814e5f15e003
harshmathurx <harshmathurx@gmail.com>
2021-07-04 20:47:47 +0530

    added keypress functionality

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i=0;i<numberOfDrumButtons;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",playAudio);
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML = this.innerHTML;
+        playAudio(buttonInnerHTML);
+    });
 }
 
-function playAudio(){
+document.addEventListener("keydown",function(event){
+    playAudio(event.key);
+});
+
+function playAudio(char){
     var crash_audio = new Audio('sounds/crash.mp3');
     var kick_audio = new Audio('sounds/kick-bass.mp3');
     var snare_audio = new Audio('sounds/snare.mp3');
@@ -11,8 +18,8 @@ function playAudio(){
     var tom_2_audio = new Audio('sounds/tom-2.mp3');
     var tom_3_audio = new Audio('sounds/tom-3.mp3');
     var tom_4_audio = new Audio('sounds/tom-4.mp3');
-    console.log(this.innerHTML);
-    switch (this.innerHTML) {
+    
+    switch (char) {
         case 'w':
             tom_1_audio.play();
             break;
